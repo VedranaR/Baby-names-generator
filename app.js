a0ed61d44c196cbeeff35f7d7f5da5bfd0e9cba2
Vedrana <vedrana_ravlic16@yahoo.com>
2018-10-26 20:32:25 +0200

    Random index func, reload, generating names

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let clear = false;
+let clear = true;
 
 window.addEventListener("DOMContentLoaded", () => {
   let girl = document.getElementById("girl");
@@ -6,10 +6,56 @@ window.addEventListener("DOMContentLoaded", () => {
   let textbox = document.getElementById("textbox");
 
   girl.addEventListener("click", () => {
-    let val = girl.value;
+    let num = getRandomNum();
+
+    fetch("girls.json")
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        let girlName = data[num];
+        if (clear) {
+          textbox.value = girlName;
+          textbox.innerText = textbox.value;
+        } else {
+          textbox.innerText = "";
+          textbox.value = girlName;
+          textbox.innerText = textbox.value;
+        }
+      })
+      .catch(function(err) {
+        console.log(err);
+      });
   });
 
   boy.addEventListener("click", () => {
-    let val = boy.value;
+    let num = getRandomNum();
+
+    fetch("boys.json")
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        let boyName = data[num];
+        if (clear) {
+          textbox.value = boyName;
+          textbox.innerText = textbox.value;
+        } else {
+          textbox.innerText = "";
+          textbox.value = boyName;
+          textbox.innerText = textbox.value;
+        }
+      })
+      .catch(function(err) {
+        console.log(err);
+      });
   });
+
+  document.getElementById("new-round").addEventListener("mousedown", e => {
+    window.location.reload();
+  });
+
+  function getRandomNum() {
+    return Math.floor(Math.random() * 56 + 1);
+  }
 });
