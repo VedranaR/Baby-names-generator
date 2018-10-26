0e179666e82891722726aeac7b41eb2c0392f808
VedranaR <vedrana_ravlic16@yahoo.com>
2018-10-26 09:51:10 +0200

    Defined variables, added evtlisteners

---
+++ app.js
@@ -0,0 +1,15 @@
+let clear = false;
+
+window.addEventListener("DOMContentLoaded", () => {
+  let girl = document.getElementById("girl");
+  let boy = document.getElementById("boy");
+  let textbox = document.getElementById("textbox");
+
+  girl.addEventListener("click", () => {
+    let val = girl.value;
+  });
+
+  boy.addEventListener("click", () => {
+    let val = boy.value;
+  });
+});
